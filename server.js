0d8d01adcf263a2a6a6a8ccdd6079f70414f0ac1
sagarbhirde <sagarbhirde111@gmail.com>
2023-03-27 22:56:27 -0500

    inital commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://mongosample:OAKVKSwTLdnUjyO1@test.xa9lb2l.mongodb.net/sample_analytics?retryWrites=true&w=majority', { useNewUrlParser :true})
 const db = mongoose.connection
 db.on('error',(error) => console.error(error))
-db.once('open', () => console.log('Connected to database'))
+db.once('open', () => console.log('Connected to database')) 
 
 
 app.use(express.json())
